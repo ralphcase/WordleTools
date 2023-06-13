44ed7c1889dd0f646cdff9117b2fed27c0710c69
Ralph Case <ralphcase@hotmail.com>
2023-06-13 16:45:57 -0400

    Add Global file to run all junit tests.

--- Wordle/src/AllTests.java
+++ Wordle/src/AllTests.java
@@ -3,7 +3,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({})
+@SuiteClasses({ PositionTest.class, ReportTest.class, SolverTest.class })
 public class AllTests {
 
 }
