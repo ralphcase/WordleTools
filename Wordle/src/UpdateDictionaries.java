ff2bb4128e0813aa024b802fe8605467d69deb86
Ralph Case <ralphcase@hotmail.com>
2023-12-31 08:02:41 -0500

    Sort letter frequency result.

--- Wordle/src/UpdateDictionaries.java
+++ Wordle/src/UpdateDictionaries.java
@@ -1,12 +1,16 @@
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.HashMap;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class UpdateDictionaries {
 
 	public static void main(String[] args) {
-		debug();
-//		System.out.println(letterFrequency(Position.getGOALWORDS()));
+//		debug();
+		System.out.println(letterFrequency(Position.getGOALWORDS()));
 	}
 
 	private static void debug() {
@@ -36,7 +40,13 @@ public class UpdateDictionaries {
 			
 		}
 	
-	public static Map<Character, Integer> letterFrequency(List<Position> words) {
+	static class EntryCompare implements Comparator<Entry<Character, Integer>> {
+	    public int compare(Entry<Character, Integer> o1, Entry<Character, Integer> o2) {
+	        return o2.getValue().compareTo(o1.getValue());
+	    }
+	}
+	
+	public static List<Entry<Character, Integer>> letterFrequency(List<Position> words) {
 		Map<Character, Integer> result = new HashMap<Character, Integer>();
 		for (Position word : words) {
 			for (Character c : word.toCharArray()) {
@@ -44,7 +54,9 @@ public class UpdateDictionaries {
 				result.put(c, num + 1);
 			}
 		}
-		return result;
+		List<Map.Entry<Character, Integer>> list = new ArrayList<Map.Entry<Character, Integer>>(result.entrySet());
+		Collections.sort(list, new EntryCompare());
+		return list;
 	}
 
 }
