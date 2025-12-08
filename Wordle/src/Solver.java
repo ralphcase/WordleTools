ad70e69cf493d6eaa9d308f6e25017a45471fece
Ralph Case <ralphcase@hotmail.com>
2025-12-08 07:34:29 -0500

    Daily wordlebot dictionary updates

--- Wordle/src/Solver.java
+++ Wordle/src/Solver.java
@@ -51,9 +51,9 @@ public class Solver {
 //				true;
 				false;
 		
-		guesses.add(new Guess(new Position("OATER"), new Report(new ArrayList<String>(List.of("gray", "gray", "yellow", "yellow", "yellow")))));
-		guesses.add(new Guess(new Position("CHERT"), new Report(new ArrayList<String>(List.of("gray", "gray", "yellow", "yellow", "green")))));
-//		guesses.add(new Guess(new Position("DHOLS"), new Report(new ArrayList<String>(List.of("gray", "yellow", "yellow", "yellow", "gray")))));
+		guesses.add(new Guess(new Position("OATER"), new Report(new ArrayList<String>(List.of("gray", "yellow", "gray", "gray", "yellow")))));
+		guesses.add(new Guess(new Position("SULKY"), new Report(new ArrayList<String>(List.of("gray", "gray", "gray", "gray", "green")))));
+//		guesses.add(new Guess(new Position("LICIT"), new Report(new ArrayList<String>(List.of("yellow", "gray", "gray", "green", "yellow")))));
 //		guesses.add(new Guess(new Position("HOLEY"), new Report(new ArrayList<String>(List.of("green", "green", "yellow", "green", "gray")))));
 //		guesses.add(new Guess(new Position("CLOMP"), new Report(new ArrayList<String>(List.of("gray", "yellow", "green", "gray", "yellow")))));
 
