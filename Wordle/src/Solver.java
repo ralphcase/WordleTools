d70f37dc770120b0a7360af3befa26745775f7c2
Ralph Case <ralphcase@hotmail.com>
2026-01-15 08:38:50 -0500

    Solutions update

--- Wordle/src/Solver.java
+++ Wordle/src/Solver.java
@@ -51,9 +51,9 @@ public class Solver {
 //				true;
 				false;
 		
-		guesses.add(new Guess(new Position("OATER"), new Report(new ArrayList<String>(List.of("gray", "yellow", "green", "gray", "green")))));
-//		guesses.add(new Guess(new Position("POIND"), new Report(new ArrayList<String>(List.of("green", "yellow", "gray", "gray", "gray")))));
-//		guesses.add(new Guess(new Position("FLUMP"), new Report(new ArrayList<String>(List.of("gray", "gray", "gray", "gray", "yellow")))));
+		guesses.add(new Guess(new Position("OATER"), new Report(new ArrayList<String>(List.of("gray", "yellow", "gray", "gray", "gray")))));
+		guesses.add(new Guess(new Position("SLICK"), new Report(new ArrayList<String>(List.of("yellow", "gray", "gray", "yellow", "gray")))));
+//		guesses.add(new Guess(new Position("WHARF"), new Report(new ArrayList<String>(List.of("gray", "gray", "green", "green", "gray")))));
 //		guesses.add(new Guess(new Position("MYRRH"), new Report(new ArrayList<String>(List.of("gray", "yellow", "yellow", "gray", "gray")))));
 
 //		guesses.add(new Guess(new Position("FUZZY"), new Report(new ArrayList<String>(List.of("gray", "gray", "gray", "gray", "gray")))));
