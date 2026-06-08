479b34bb7113ce3ab5c3b21ca814f0fd84ea0b77
Ralph Case <ralphcase@hotmail.com>
2026-06-08 06:54:48 -0400

    Daily solutions.txt update

--- Wordle/src/main/java/app/Main.java
+++ Wordle/src/main/java/app/Main.java
@@ -21,13 +21,13 @@ public class Main {
         WordRepository repo = initializer.loadDictionaries();
 
         boolean hard = false;
-        Solver solver = new Solver(repo, hard, Solver.Mode.ARCHIVE);
-//        Solver solver = new Solver(repo, hard, Solver.Mode.NEW);
+//        Solver solver = new Solver(repo, hard, Solver.Mode.ARCHIVE);
+        Solver solver = new Solver(repo, hard, Solver.Mode.NEW);
 //        Solver solver = new Solver(repo, hard, Solver.Mode.ALL);
 
-        solver.applyFeedback(new Word("IRATE"), Feedback.of(gray, gray, gray, gray, green));
-        solver.applyFeedback(new Word("LOUSE"), Feedback.of(gray, green, gray, gray, green));
-//        solver.applyFeedback(new Word("BUTCH"), Feedback.of(yellow, yellow, yellow, gray, yellow));
+        solver.applyFeedback(new Word("SANER"), Feedback.of(gray, green, gray, gray, gray));
+        solver.applyFeedback(new Word("MALTY"), Feedback.of(green, green, gray, gray, gray));
+        solver.applyFeedback(new Word("MAMBA"), Feedback.of(green, green, gray, gray, green));
 //        solver.applyFeedback(new Word("WISER"), Feedback.of(ABSENT, CORRECT, CORRECT, CORRECT, CORRECT));
 
         // Print remaining candidates
