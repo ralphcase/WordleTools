b24430c8315e06b28773a073a312742f5c642588
Ralph Case <ralphcase@hotmail.com>
2026-07-22 07:10:42 -0400

    daily solutions.txt update

--- Wordle/src/main/java/app/Main.java
+++ Wordle/src/main/java/app/Main.java
@@ -35,11 +35,11 @@ public class Main {
         Solver solver = new Solver(repo, hard, Solver.Mode.NEW);
 //        Solver solver = new Solver(repo, hard, Solver.Mode.ALL);
 
-        solver.applyFeedback(new Word("SANER"), Feedback.of(green, gray, gray, gray, gray));
+        solver.applyFeedback(new Word("SANER"), Feedback.of(gray, gray, gray, gray, yellow));
         DictionaryBuilder.predictWordlebot(solver.remainingCandidates());
 
-        solver.applyFeedback(new Word("THIOL"), Feedback.of(gray, green, green, gray, green));
-//        solver.applyFeedback(new Word("COUTH"), Feedback.of(gray, green, gray, green, green));
+        solver.applyFeedback(new Word("CROUP"), Feedback.of(gray, yellow, yellow, gray, gray));
+        solver.applyFeedback(new Word("LORDY"), Feedback.of(green, green, green, gray, green));
 //        solver.applyFeedback(new Word("PUDIC"), Feedback.of(gray, yellow, gray, gray, gray));
 
         // Print remaining candidates
