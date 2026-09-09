b137532fcfeddbc7a08ccd5bf857ea33b17f36b3
Ralph Case <ralphcase@hotmail.com>
2026-09-09 09:38:27 -0400

    Daily solutions update

--- Wordle/src/main/java/app/Main.java
+++ Wordle/src/main/java/app/Main.java
@@ -35,10 +35,10 @@ public class Main {
         Solver solver = new Solver(repo, hard, Solver.Mode.NEW);
 //        Solver solver = new Solver(repo, hard, Solver.Mode.ALL);
 
-        solver.applyFeedback(new Word("SANER"), Feedback.of(gray, gray, yellow, green, gray));
+        solver.applyFeedback(new Word("SANER"), Feedback.of(yellow, gray, gray, gray, yellow));
         DictionaryBuilder.predictWordlebot(solver.remainingCandidates());
 
-        solver.applyFeedback(new Word("INKED"), Feedback.of(yellow, yellow, gray, green, gray));
+        solver.applyFeedback(new Word("ROUST"), Feedback.of(yellow, gray, yellow, green, green));
 //        solver.applyFeedback(new Word("REBID"), Feedback.of(green, green, gray, green, gray));
 //        solver.applyFeedback(new Word("FUTON"), Feedback.of(gray, yellow, yellow, gray, yellow));
 //        solver.applyFeedback(new Word("FLICK"), Feedback.of(gray, green, green, green, green));
